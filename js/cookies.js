92bc2dfdcaf35a2b0a15ea32a55833607880bdf5
Zackery Hatch <zackery.hatch@yahoo.com>
2020-04-13 17:52:47 -0700

    Fixed the cookies to push to a year ahead of current date

--- js/cookies.js
+++ js/cookies.js
@@ -1,110 +1,129 @@
-var userFirstName, userLastName, user, d, m, time, cookieValue, userInformation, cookieInfoUpperStr, windowInfo, log, keysPressed, leavingInfo, userMessage, ipLook, ipLookLong, dataX, appliedData;
+var userFirstName,
+	userLastName,
+	user,
+	d,
+	m,
+	time,
+	cookieValue,
+	userInformation,
+	cookieInfoUpperStr,
+	windowInfo,
+	log,
+	keysPressed,
+	leavingInfo,
+	userMessage,
+	ipLook,
+	ipLookLong,
+	dataX,
+	appliedData;
 
 function cookieString(len, charSet) {
-    charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_+=-`';
-    var cookieString = '';
-    for (var i = 0; i < len; i++) {
-        var randomPoz = Math.floor(Math.random() * charSet.length);
-        cookieString += charSet.substring(randomPoz, randomPoz + 1);
-    }
-    return cookieString;
+	charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_+=-`';
+	var cookieString = '';
+	for (var i = 0; i < len; i++) {
+		var randomPoz = Math.floor(Math.random() * charSet.length);
+		cookieString += charSet.substring(randomPoz, randomPoz + 1);
+	}
+	return cookieString;
 }
 
 function cookie() {
-    ipInformation();
-    cookieValue = cookieString(32);
-    document.cookie = 'cookie=' + cookieValue + ';expires=Thu, 26 Mar 2020 15:26:23 GMT;path=/';
-    // User's Input Information
-    userFirstName = window.prompt('Please enter your first name: ');
-    userLastName = window.prompt('Please enter your last name: ');
-    user = { first_name: userFirstName, last_name: userLastName };
+	ipInformation();
+	cookieValue = cookieString(8);
 
-    // User's given data
-    d = new Date();
-    m = {
-        0: 'Jan',
-        1: 'Feb',
-        2: 'March',
-        3: 'April',
-        4: 'May',
-        5: 'June',
-        6: 'Jul',
-        7: 'Aug',
-        8: 'Sept',
-        9: 'Oct',
-        10: 'Nov',
-        11: 'Dec'
-    };
-    time = {
-        year: d.getFullYear(),
-        month: m[d.getMonth()],
-        day: d.getDay(),
-        hour: d.getHours(),
-        minutes: d.getMinutes(),
-        seconds: d.getSeconds()
-    };
-    ///////////////////////////// Extra Info
-    h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-    w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    windowInfo = { Height: h, Width: w };
+	// User's Input Information
+	userFirstName = window.prompt('Please enter your first name: ');
+	userLastName = window.prompt('Please enter your last name: ');
+	user = { first_name: userFirstName, last_name: userLastName };
 
+	// User's given data
+	d = new Date();
+	m = {
+		0  : 'Jan',
+		1  : 'Feb',
+		2  : 'Mar',
+		3  : 'Apr',
+		4  : 'May',
+		5  : 'Jun',
+		6  : 'Jul',
+		7  : 'Aug',
+		8  : 'Sep',
+		9  : 'Oct',
+		10 : 'Nov',
+		11 : 'Dec',
+	};
+	wd = {
+		0 : 'Sun',
+		1 : 'Mon',
+		2 : 'Tue',
+		3 : 'Wed',
+		4 : 'Thu',
+		5 : 'Fri',
+		6 : 'Sat',
+	};
+	time = {
+		year    : d.getFullYear(),
+		month   : m[d.getMonth()],
+		day     : wd[d.getDay()],
+		hour    : d.getHours(),
+		minutes : d.getMinutes(),
+		seconds : d.getSeconds(),
+	};
+	///////////////////////////// Extra Info
+	h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+	w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+	windowInfo = { Height: h, Width: w };
 
-    function ipInformation() {
-        ipLook = fetch('https://ipapi.co/json/')
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                // console.log(data);
-                data = JSON.stringify(data, null, 2);
-                // console.log(data);
-                document.getElementById('ipBox').value = data;
-            });
-    }
+	document.cookie =
+		'cookie=' +
+		cookieValue +
+		';expires=' +
+		time.day +
+		', 26 ' +
+		time.month +
+		(time.year + 1) +
+		' 00:00:01 MST;path=/';
+	var testVar =
+		'cookie=' +
+		cookieValue +
+		';expires=' +
+		time.day +
+		', 26 ' +
+		time.month +
+		' ' +
+		(time.year + 1) +
+		' 00:00:01 MST;path=/';
+	console.log(testVar);
+	function ipInformation() {
+		ipLook = fetch('https://ipapi.co/json/')
+			.then(function(response) {
+				return response.json();
+			})
+			.then(function(data) {
+				// console.log(data);
+				data = JSON.stringify(data, null, 2);
+				// console.log(data);
+				document.getElementById('ipBox').value = data;
+			});
+	}
 
+	userInformation = { user_name: user, cookie_value: cookieValue, current_time: time, window_information: windowInfo };
+	// console.log(userInformation);
+	// Real browser cookie
+	cookieInfoUpperStr = JSON.stringify(userInformation); // Makes the cookie not [object Object]
 
-    userInformation = { user_name: user, cookie_value: cookieValue, current_time: time, window_information: windowInfo };
-    // console.log(userInformation);
-    // Real browser cookie
-    cookieInfoUpperStr = JSON.stringify(userInformation); // Makes the cookie not [object Object]
-
-    ///////////////////////////// End Extra Info
-
-}
-function deleteAllCookies() {
-    var cookies = document.cookie.split(";");
-
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        var eqPos = cookie.indexOf("=");
-        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    }
+	///////////////////////////// End Extra Info
 }
 
 function setVisit() {
-    // Key Logger
-    // log = document.documentElement;
-    // keysPressed = []
-    // document.addEventListener('keydown', logKey);
-    // function logKey(e) {
-    //     keysPressed.push(log = ` ${e.code}`);
-    //     console.log(keysPressed);
-    // }
-    // End key - logger
-    if (document.cookie != document.cookie || document.cookie.length < 1) {
-        cookie();
-        document.getElementById('emailBox').value = 'DNR@robot.com';
-        document.getElementById('textarea').value = cookieInfoUpperStr;
-        jQuery(document).ready(function () {
-            setTimeout(function () {
-                document.getElementById('submit').click();
-            }, 50);
-        });
-    }
-    // else if (document.cookie != cookieValue) {
-    //     deleteAllCookies();
-    //     console.log("All cookies deleted.")
-    //     cookie();
-    // }
+	if (document.cookie != document.cookie || document.cookie.length < 1) {
+		cookie();
+		document.getElementById('emailBox').value = 'DNR@robot.com';
+		document.getElementById('textarea').value = cookieInfoUpperStr;
+		jQuery(document).ready(function() {
+			setTimeout(function() {
+				document.getElementById('submit').click();
+			}, 100);
+		});
+	}
 }
